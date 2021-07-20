9a4d88449dfb23787cb8d4e87da845983f15726c
Glen <mistergjones@yahoo.com.au>
2021-07-20 15:53:19 +1000

    more upates

---
+++ src/FYI/fyi.js
@@ -0,0 +1,21 @@
+// lottie files to consider
+
+// just dogs
+/*
+https://lottiefiles.com/56997-dog-walking
+https://lottiefiles.com/14751-dog-helpet
+https://lottiefiles.com/16780-dog2
+https://lottiefiles.com/10607-dog-avatar
+https://lottiefiles.com/69263-my-dog-and-me
+https://lottiefiles.com/53281-a-dog
+*/
+
+// dog walkers
+/*
+https://lottiefiles.com/16984-boy-and-doggy
+https://lottiefiles.com/20586-illustration-walking-people-with-dog-thinking-wine
+https://lottiefiles.com/66887-walking-with-dog
+http://lottiefiles.com/go/premium-animation/59877
+https://lottiefiles.com/69483-slipper
+http://lottiefiles.com/go/premium-animation/38031
+*/
